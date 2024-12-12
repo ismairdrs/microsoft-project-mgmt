64860a00a1262c7b0f097c6a779c24ad4646f57d
ismairdrs <ismairdrs@outlook.com>
2024-12-12 16:24:59 -0300

    create base model

---
+++ microsoft/app/base_model.py
@@ -0,0 +1,25 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import Mapped, mapped_column
+
+from gringotts.db import Base
+from gringotts.db.functions import utcnow
+
+
+class BaseModel(Base):
+    __abstract__ = True
+    
+    id: Mapped[uuid.UUID] = mapped_column(
+        postgresql.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=utcnow()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=utcnow(),
+        onupdate=utcnow(),
+    )
